05bacbfb7e889b512c34ea6bb19f9ef739d45a9f
irieryota <ryotairie123@gmail.com>
2019-07-01 02:06:29 +0900

    vga_text_bufferに文字を出力するところまで

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+#![no_std]
+#![no_main]
+
+mod vga_buffer;
+
+use core::panic::PanicInfo;
+
+static HELLO: &[u8] = b"Hello World!";
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}",info);
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    println!("jello hogr");
+    loop{}
+}
